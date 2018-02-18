7cd4dbca3ca55f5f6d2d44eb9bb7eb0fa8bf0899
mhd53 <mhd53@users.noreply.github.com>
2018-02-18 02:51:07 -0500

    Update trying out bootstrap carousel - fix

--- project_page/js/imageView.js
+++ project_page/js/imageView.js
@@ -9,11 +9,20 @@ $(function() {
 
 		var photo = $("<img>");
 		photo.addClass("photo");
+		// photo.addClass("rounded mx-auto d-block");
 		photo.attr('src', link);
 		imageViewer.append(photo);
 
 		// Add image view to DOM
+		// $(this).parents(".app-desc").append(imageViewer);
+		// $(this).parents().eq(3).append(imageViewer);
+		//$("#berk").append(imageViewer);
+		// $(this).append(imageViewer);
+		// $(".app-desc").eq(2).append(imageViewer);
 		$(".main-content").append(imageViewer);
+		// $(".container").append(imageViewer);
+		// $("#overlay").append(imageViewer);
+		// $("#overlay").css("display", "block");
 		imageViewer.fadeTo(500, 1);
 
 		// Set event to close image viewer
@@ -22,7 +31,26 @@ $(function() {
 			$(this).fadeTo(500, 0, function() 
 			{
 				$(this).remove();
+				// $("#overlay").css("display", "none");
 			});
 		});
 	});
 })
+
+function scaleImg(imageToScale) {
+	// Get body dimensions
+	bodyWidth = $(document.body).width();
+	bodyHeight = $(document.body).height();
+
+	// Debug
+	console.log("Body Width: " + bodyWidth);
+	console.log("Body Height: " + bodyHeight);
+
+	// Scale image relative to width & height of document
+	$(imageToScale).css(
+			{
+				"width" : bodyWidth /2,
+				"height" : bodyHeight
+			});
+}       
+
