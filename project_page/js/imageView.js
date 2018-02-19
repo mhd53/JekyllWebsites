1dc1051cc466bd463063233c109bb1bbe5321d02
mhd53 <mhd53@users.noreply.github.com>
2018-02-19 00:10:23 -0500

    Fixed overlay image & removed carousel -bootstrap

--- project_page/js/imageView.js
+++ project_page/js/imageView.js
@@ -14,15 +14,7 @@ $(function() {
 		imageViewer.append(photo);
 
 		// Add image view to DOM
-		// $(this).parents(".app-desc").append(imageViewer);
-		// $(this).parents().eq(3).append(imageViewer);
-		//$("#berk").append(imageViewer);
-		// $(this).append(imageViewer);
-		// $(".app-desc").eq(2).append(imageViewer);
 		$(".main-content").append(imageViewer);
-		// $(".container").append(imageViewer);
-		// $("#overlay").append(imageViewer);
-		// $("#overlay").css("display", "block");
 		imageViewer.fadeTo(500, 1);
 
 		// Set event to close image viewer
@@ -31,26 +23,7 @@ $(function() {
 			$(this).fadeTo(500, 0, function() 
 			{
 				$(this).remove();
-				// $("#overlay").css("display", "none");
 			});
 		});
 	});
 })
-
-function scaleImg(imageToScale) {
-	// Get body dimensions
-	bodyWidth = $(document.body).width();
-	bodyHeight = $(document.body).height();
-
-	// Debug
-	console.log("Body Width: " + bodyWidth);
-	console.log("Body Height: " + bodyHeight);
-
-	// Scale image relative to width & height of document
-	$(imageToScale).css(
-			{
-				"width" : bodyWidth /2,
-				"height" : bodyHeight
-			});
-}       
-
